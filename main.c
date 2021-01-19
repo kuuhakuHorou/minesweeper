8da95e52f04660e9b000aaf3dca5c469c1a33442
kuuhakuHorou <63546221+kuuhakuHorou@users.noreply.github.com>
2021-01-19 12:38:15 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "funtion.h"
 
 int main() {
-    system("title ½ò¦a¹pv2.0 ");
+    system("title Â½Ã²Â¦aÂ¹pv2.0.0 ");
     system("mode con cols=120 lines=36");
     game_dif_choose();
     system("pause");
