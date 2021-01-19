8abde3504bdf79ee42d327a8ccac262be952e3e2
kuuhakuHorou <63546221+kuuhakuHorou@users.noreply.github.com>
2021-01-19 12:19:54 +0800

    Add files via upload

---
+++ funt_dif_3.c
@@ -0,0 +1,343 @@
+#include "funtion.h"
+
+int map_3[LINES_HARD+2][COLS_HARD+2];       //原始地圖
+int map_guess_3[LINES_HARD+2][COLS_HARD+2]; //遊玩者使用的地圖
+int map_mark_3[LINES_HARD][COLS_HARD]={0};  //標記地圖
+int error_enter_3;    //錯誤輸入
+int game_over_3;      //遊戲結束
+int flag_num_3=0;     //標記數量
+
+void game_hard() {   //遊戲內容
+    int i, j, mod, mod_choose;
+    game_over_3=0;
+    game_start_hard();
+    print_game_hard();
+    while (1) {
+        if (error_enter_3==0) {
+            gotoxy(45,1);
+            printf("請輸入你想做什麼(1:\"踩\"，2:標記): ");
+            for (i=0;i<10;i++)
+                putchar(' ');
+            for (i=0;i<10;i++)
+                putchar('\b');
+            scanf("%d", &mod);
+            fflush(stdin);  //清除輸入緩衝區，以防亂輸入
+            switch (mod) {
+                case 1:
+                    mod_choose=1;
+                    break;
+                case 2:
+                    mod_choose=2;
+                    break;
+                default:
+                    gotoxy(50,5);
+                    printf("輸入錯誤!請重新輸入!!");
+                    error_enter_3=1;
+                    mod_choose=0;   //一開始沒輸入錯誤但後面有時，防止跳入上一個選則
+                    Sleep(3000);
+                    gotoxy(50,5);
+                    for (i=0;i<21;i++)
+                        putchar(' ');
+                    for (i=0;i<21;i++)
+                        putchar('\b');
+                    break;
+            }
+            if (mod_choose==1) {
+                mod_choose_1_hard();
+            }
+            else if (mod_choose==2) {
+                mod_choose_2_hard();
+            }
+        }
+        else if (error_enter_3==1) {
+            error_enter_3=0;
+            if (mod_choose==1)
+                mod_choose_1_hard();
+            else if (mod_choose==2)
+                mod_choose_2_hard();
+        }
+        
+        if (game_over_3==1) {
+            gotoxy(50,11);
+            break;
+        }
+    }
+}
+
+void game_start_hard() {     //遊戲開始設定
+    int i, j, enter_x, enter_y, error;
+    for (i=0;i<LINES_HARD+2;i++) {
+        for (j=0;j<COLS_HARD+2;j++) {
+            if (i==0||i==LINES_HARD+1||j==0||j==COLS_HARD+1)
+                map_3[i][j]=99;   //99為邊界
+            else 
+                map_3[i][j]=0;    //0為空地
+            map_guess_3[i][j]=map_3[i][j];
+        }
+    }
+    while (1) {
+        error=0;
+        print_game_hard();
+        gotoxy(45,1);
+        printf("請先輸入一個座標(x y): ");
+        scanf("%d%d", &enter_y, &enter_x);
+        fflush(stdin);
+        if (enter_x>COLS_HARD||enter_x<1||enter_y>LINES_HARD||enter_y<1) {
+            error=1;
+            gotoxy(50,5);
+            printf("請不要亂輸入喔!");
+            Sleep(3000);
+        }
+        if (error!=1)
+            break;
+    }
+    map_guess_3[enter_y][enter_x]=10;
+    landboom_generate_hard(&enter_x,&enter_y);    //生成地雷
+    landboom_tester_hard(enter_x,enter_y);
+}
+
+void landboom_generate_hard(int *enter_x,int *enter_y) {  //隨機生成地雷
+    srand(time(0));
+    int i, j, produce_x, produce_y, landboom_num=0;
+    do {
+        produce_x=rand()%COLS_HARD+1;
+        produce_y=rand()%LINES_HARD+1;
+        if (map_3[produce_y][produce_x]!=20&&produce_x!=*enter_x&&produce_y!=*enter_y) {
+            map_3[produce_y][produce_x]=20;   //20為地雷
+            landboom_num++;
+            map_guess_3[produce_y][produce_x]=map_3[produce_y][produce_x];
+        }
+    } while (landboom_num<99);
+/*    for (int i=0;i<landboom_num;i++) {
+ *       produce_x=rand()%COLS_EASY+1;
+ *       produce_y=rand()%LINES_EASY+1;
+ *       if (map_1[produce_y][produce_x]==20)  //已經為地雷的情況
+ *           i--;
+ *       map_1[produce_y][produce_x]=20;   //20為地雷
+ *   }
+ */
+}
+
+void landboom_tester_hard(int x,int y) { //測試旁邊八格地雷
+    int i, j, landboom;
+    landboom=0; //重算地雷數
+    
+    for (i=y-1;i<=y+1;i++) {        //測試周邊地雷
+        for (j=x-1;j<=x+1;j++) {
+            if (i==y&&j==x)     //踩的那格不用算
+                continue;
+            else if (map_3[i][j]==20)
+                landboom++;
+        }
+    }
+    
+    if (landboom==0) {
+        map_guess_3[y][x]=10;   //旁邊沒地雷
+        for (i=y-1;i<=y+1;i++) {
+            for (j=x-1;j<=x+1;j++) {
+                if (map_guess_3[i][j]==0)     //還沒踩過的地方
+                    landboom_tester_hard(j,i);
+            }
+        }
+    }
+    else
+        map_guess_3[y][x]=landboom;  //旁邊有地雷(輸出多少)
+}
+
+void mod_choose_1_hard() {   //"踩"
+    int i, j, guess_x, guess_y, untreated_items;
+    gotoxy(45,2);
+    printf("請輸入你想踩哪(x y): ");
+    for (i=0;i<10;i++)
+        putchar(' ');
+    for (i=0;i<10;i++)
+        putchar('\b');
+    scanf("%d%d", &guess_y, &guess_x);
+    fflush(stdin);  //清除輸入緩衝區，以防亂輸入
+    if (guess_x>COLS_HARD||guess_x<1||guess_y>LINES_HARD||guess_y<1) {    //超出地雷範圍
+        gotoxy(50,5);
+        printf("輸入錯誤!請重新輸入!!");
+        error_enter_3=1;
+        Sleep(3000);
+        gotoxy(50,5);
+        for (i=0;i<21;i++)
+            putchar(' ');
+        for (i=0;i<21;i++)
+            putchar('\b');
+        return;
+    }
+    else if (map_mark_3[guess_y-1][guess_x-1]==1) {
+        gotoxy(50,5);
+        printf("你不能踩標記的地喔!");
+        error_enter_3=1;
+        Sleep(3000);
+        gotoxy(50,5);
+        for (i=0;i<21;i++)
+            putchar(' ');
+        for (i=0;i<21;i++)
+            putchar('\b');
+        return;
+    }
+    else if (map_guess_3[guess_y][guess_x]==20) {
+        for (i=0;i<LINES_HARD+2;i++) {
+            for (j=0;j<COLS_HARD+2;j++) {
+                if (map_guess_3[i][j]==20)
+                    map_guess_3[i][j]++;  //輸了以顯示地雷處
+                else if (map_mark_3[i-1][j-1]==1&&map_guess_3[i][j]<20)
+                    map_mark_3[i-1][j-1]=2;   //標記錯誤，把錯誤顯現出來
+            }
+        }
+        print_game_hard();
+        gotoxy(50,10);
+        printf("你輸了");
+        game_over_3=1;
+        return;
+    }
+    landboom_tester_hard(guess_x,guess_y);
+    print_game_hard();
+    untreated_items=0;  //重算沒踩過的地
+    for (i=0;i<LINES_HARD+2;i++) {
+        for (j=0;j<COLS_HARD+2;j++) {
+            if (map_guess_3[i][j]==0) {
+                untreated_items++;
+            }
+            else if (map_guess_3[i][j]<20) {
+                continue;   //踩過且不算地雷處
+            }
+        }
+    }
+    if (untreated_items==0) {
+        for (i=0;i<LINES_HARD+2;i++) {
+            for (j=0;j<COLS_HARD+2;j++) {
+                if (map_guess_3[i][j]==20)
+                    map_guess_3[i][j]=30; //顯現地雷處
+            }
+        }
+        print_game_hard();
+        gotoxy(50,10);
+        printf("你贏了");
+        game_over_3=1;
+        return;
+    }
+}
+
+void mod_choose_2_hard() {   //標記
+    int i, mark_x, mark_y;
+    gotoxy(45,2);
+    printf("請輸入你想標記哪(x y): ");
+    for (i=0;i<10;i++)
+        putchar(' ');
+    for (i=0;i<10;i++)
+        putchar('\b');
+    scanf("%d%d", &mark_y, &mark_x);
+    fflush(stdin);  //清除輸入緩衝區，以防亂輸入
+    if (mark_x>COLS_HARD||mark_x<1||mark_y>LINES_HARD||mark_y<1) {//超出地雷範圍
+        gotoxy(50,5);
+        printf("輸入錯誤!請重新輸入!!");
+        error_enter_3=1;
+        Sleep(3000);
+        gotoxy(50,5);
+        for (i=0;i<21;i++)
+            putchar(' ');
+        for (i=0;i<21;i++)
+            putchar('\b');
+        return;
+    }
+    else if (map_guess_3[mark_y][mark_x]==20||map_guess_3[mark_y][mark_x]==0) {
+        if (map_mark_3[mark_y-1][mark_x-1]==0) {
+            map_mark_3[mark_y-1][mark_x-1]=1; //標記
+            flag_num_3++;
+            print_game_hard();
+        }
+        else if (map_mark_3[mark_y-1][mark_x-1]==1) {
+            map_mark_3[mark_y-1][mark_x-1]=0; //清除標記
+            flag_num_3--;
+            print_game_hard();
+        }
+    }
+    else
+        print_game_hard();
+}
+
+void print_game_hard() { //介面
+    system("cls");
+    int i, j;
+    printf("　　 y→\n　　　");
+    for (i=1;i<=16;i++)
+        printf("%2d", i);
+    printf("\n x\n↓");
+    for (i=1;i<=30;i++) {
+        gotoxy(3,i+3);
+        printf("%2d", i);
+    }
+/*  printf("　 　y→\n"        原輸出
+ *         "　　　 1 2 3 4 5 6 7 8 910111213141516\n"
+ *         " x　\n"
+ *         "↓ 1\n"
+ *         "　 2\n"
+ *         "　 3\n"
+ *         "　 4\n"
+ *         "　 5\n"
+ *         "　 6\n"
+ *         "　 7\n"
+ *         "　 8\n"
+ *         "　 9\n"
+ *         "　10\n"
+ *         "　11\n"
+ *         "　12\n"
+ *         "　13\n"
+ *         "　14\n"
+ *         "　15\n"
+ *         "　16\n"
+ *         "　17\n"
+ *         "　18\n"
+ *         "　19\n"
+ *         "　20\n"
+ *         "　21\n"
+ *         "　22\n"
+ *         "　23\n"
+ *         "　24\n"
+ *         "　25\n"
+ *         "　26\n"
+ *         "　27\n"
+ *         "　28\n"
+ *         "　29\n"
+ *         "　30\n");
+ */
+    gotoxy(14,1);
+    printf("地雷數:99");
+    gotoxy(25,1);
+    printf("旗子數:%2d", flag_num_3);
+    for (i=0;i<LINES_HARD+2;i++) {
+        gotoxy(5,i+3);
+        for (j=0;j<COLS_HARD+2;j++) {
+            if (map_guess_3[i][j]==99)
+                printf("■");         //邊界
+            else if (map_guess_3[i][j]==10)
+                printf("　");    //踩過的空地
+            else if (map_guess_3[i][j]==0||map_guess_3[i][j]==20) {
+                if (map_mark_3[i-1][j-1]==0)
+                    printf("□");     //還沒踩過的地面&地雷
+                else if (map_mark_3[i-1][j-1]==1)
+                    printf("◢");    //地雷標記顯現
+                else if (map_mark_3[i-1][j-1]==2)
+                    printf("Χ");     //標記錯誤顯現
+            }
+            else if (map_guess_3[i][j]<10)
+                printf(" %d", map_guess_3[i][j]); //踩過且旁邊有地雷，顯現地雷數
+            else if (map_guess_3[i][j]==21) {
+                if (map_mark_3[i-1][j-1]==0)
+                    printf("※");    //全場地雷處
+                else if (map_mark_3[i-1][j-1]==1)
+                    printf("◢");    //地雷標記顯現
+            }
+            else if (map_guess_3[i][j]==30) {
+                if (map_mark_3[i-1][j-1]==0)
+                    printf("⊕");    //過關顯示地雷
+                else if (map_mark_3[i-1][j-1]==1)
+                    printf("◢");    //地雷標記顯現
+            }
+        }
+    }
+    putchar('\n');
+}
--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include "funtion.h"
 
 int main() {
+    system("title ½ò¦a¹pv2.0 ");
     system("mode con cols=120 lines=36");
-    game();
+    game_dif_choose();
     system("pause");
     return 0;
 }
