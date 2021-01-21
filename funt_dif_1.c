fcd24bc0e06c6cf38491f316f083311bf42076f7
kuuhakuHorou <63546221+kuuhakuHorou@users.noreply.github.com>
2021-01-21 12:14:45 +0800

    Add files via upload

--- funt_dif_1.c
+++ funt_dif_1.c
@@ -1,19 +1,15 @@
 #include "funtion.h"
 
-int map_1[LINES_EASY+2][COLS_EASY+2];       //原始地圖
-int map_guess_1[LINES_EASY+2][COLS_EASY+2]; //遊玩者使用的地圖
+int map_1[LINES_EASY+2][COLS_EASY+2];       //地圖
 int map_mark_1[LINES_EASY][COLS_EASY]={0};  //標記地圖
-int error_enter_1;    //錯誤輸入
-int game_over_1;      //遊戲結束
-int flag_num_1=0;     //旗子數量
 
 void game_easy() {   //遊戲內容
     int i, j, mod, mod_choose;
-    game_over_1=0;
+    game.over=0;
     game_start_easy();
     print_game_easy();
     while (1) {
-        if (error_enter_1==0) {
+        if (game.error==0) {
             gotoxy(40,1);
             printf("請輸入你想做什麼(1:\"踩\"，2:標記): ");
             for (i=0;i<10;i++)
@@ -34,14 +30,12 @@ void game_easy() {   //遊戲內容
                     color_set(12);
                     printf("輸入錯誤!請重新輸入!!");
                     color_set(7);
-                    error_enter_1=1;
+                    game.error=1;
                     mod_choose=0;   //一開始沒輸入錯誤但後面有時，防止跳入上一個選則
                     Sleep(3000);
                     gotoxy(45,5);
                     for (i=0;i<21;i++)
                         putchar(' ');
-                    for (i=0;i<21;i++)
-                        putchar('\b');
                     break;
             }
             if (mod_choose==1) {
@@ -51,15 +45,15 @@ void game_easy() {   //遊戲內容
                 mod_choose_2_easy();
             }
         }
-        else if (error_enter_1==1) {
-            error_enter_1=0;
+        else if (game.error==1) {
+            game.error=0;
             if (mod_choose==1)
                 mod_choose_1_easy();
             else if (mod_choose==2)
                 mod_choose_2_easy();
         }
         
-        if (game_over_1==1) {
+        if (game.over==1) {
             gotoxy(45,11);
             break;
         }
@@ -74,7 +68,6 @@ void game_start_easy() {     //遊戲開始設定
                 map_1[i][j]=99;   //99為邊界
             else 
                 map_1[i][j]=0;    //0為空地
-            map_guess_1[i][j]=map_1[i][j];
         }
     }
     while (1) {
@@ -95,7 +88,7 @@ void game_start_easy() {     //遊戲開始設定
         if (error!=1)
             break;
     }
-    map_guess_1[enter_y][enter_x]=10;
+    map_1[enter_y][enter_x]=10;
     landboom_generate_easy(&enter_x,&enter_y);    //生成地雷
     landboom_tester_easy(enter_x,enter_y);
 }
@@ -109,7 +102,6 @@ void landboom_generate_easy(int *enter_x, int *enter_y) {  //隨機生成地雷
         if (map_1[produce_y][produce_x]!=20&&produce_x!=*enter_x&&produce_y!=*enter_y) {
             map_1[produce_y][produce_x]=20;   //20為地雷
             landboom_num++;
-            map_guess_1[produce_y][produce_x]=map_1[produce_y][produce_x];
         }
     } while (landboom_num<10);
 /*    for (int i=0;i<landboom_num;i++) {
@@ -136,16 +128,16 @@ void landboom_tester_easy(int x,int y) { //測試旁邊八格地雷
     }
     
     if (landboom==0) {
-        map_guess_1[y][x]=10;   //旁邊沒地雷
+        map_1[y][x]=10;   //旁邊沒地雷
         for (i=y-1;i<=y+1;i++) {
             for (j=x-1;j<=x+1;j++) {
-                if (map_guess_1[i][j]==0)     //還沒踩過的地方
+                if (map_1[i][j]==0)     //還沒踩過的地方
                     landboom_tester_easy(j,i);
             }
         }
     }
     else
-        map_guess_1[y][x]=landboom;  //旁邊有地雷(輸出多少)
+        map_1[y][x]=landboom;  //旁邊有地雷(輸出多少)
 }
 
 void mod_choose_1_easy() {   //"踩"
@@ -163,13 +155,11 @@ void mod_choose_1_easy() {   //"踩"
         color_set(12);
         printf("輸入錯誤!請重新輸入!!");
         color_set(7);
-        error_enter_1=1;
+        game.error=1;
         Sleep(3000);
         gotoxy(45,5);
         for (i=0;i<21;i++)
             putchar(' ');
-        for (i=0;i<21;i++)
-            putchar('\b');
         return;
     }
     else if (map_mark_1[guess_y-1][guess_x-1]==1) {
@@ -177,28 +167,26 @@ void mod_choose_1_easy() {   //"踩"
         color_set(12);
         printf("你不能踩標記的地喔!");
         color_set(7);
-        error_enter_1=1;
+        game.error=1;
         Sleep(3000);
         gotoxy(45,5);
         for (i=0;i<21;i++)
             putchar(' ');
-        for (i=0;i<21;i++)
-            putchar('\b');
         return;
     }
-    else if (map_guess_1[guess_y][guess_x]==20) {
+    else if (map_1[guess_y][guess_x]==20) {
         for (i=0;i<LINES_EASY+2;i++) {
             for (j=0;j<COLS_EASY+2;j++) {
-                if (map_guess_1[i][j]==20)
-                    map_guess_1[i][j]++;  //輸了以顯示地雷處
-                else if (map_mark_1[i-1][j-1]==1&&map_guess_1[i][j]<20)
+                if (map_1[i][j]==20)
+                    map_1[i][j]++;  //輸了以顯示地雷處
+                else if (map_mark_1[i-1][j-1]==1&&map_1[i][j]<20)
                     map_mark_1[i-1][j-1]=2;   //標記錯誤，把錯誤顯現出來
             }
         }
         print_game_easy();
         gotoxy(45,10);
         printf("你輸了");
-        game_over_1=1;
+        game.over=1;
         return;
     }
     landboom_tester_easy(guess_x,guess_y);
@@ -206,10 +194,10 @@ void mod_choose_1_easy() {   //"踩"
     untreated_items=0;  //重算沒踩過的地
     for (i=0;i<LINES_EASY+2;i++) {
         for (j=0;j<COLS_EASY+2;j++) {
-            if (map_guess_1[i][j]==0) {
+            if (map_1[i][j]==0) {
                 untreated_items++;
             }
-            else if (map_guess_1[i][j]<20) {
+            else if (map_1[i][j]<20) {
                 continue;   //踩過且不算地雷處
             }
         }
@@ -217,14 +205,14 @@ void mod_choose_1_easy() {   //"踩"
     if (untreated_items==0) {
         for (i=0;i<LINES_EASY+2;i++) {
             for (j=0;j<COLS_EASY+2;j++) {
-                if (map_guess_1[i][j]==20)
-                    map_guess_1[i][j]=30; //顯現地雷處
+                if (map_1[i][j]==20)
+                    map_1[i][j]=30; //顯現地雷處
             }
         }
         print_game_easy();
         gotoxy(45,10);
         printf("你贏了");
-        game_over_1=1;
+        game.over=1;
         return;
     }
 }
@@ -244,24 +232,22 @@ void mod_choose_2_easy() {   //標記
         color_set(12);
         printf("輸入錯誤!請重新輸入!!");
         color_set(7);
-        error_enter_1=1;
+        game.error=1;
         Sleep(3000);
         gotoxy(45,5);
         for (i=0;i<21;i++)
             putchar(' ');
-        for (i=0;i<21;i++)
-            putchar('\b');
         return;
     }
-    else if (map_guess_1[mark_y][mark_x]==20||map_guess_1[mark_y][mark_x]==0) {
+    else if (map_1[mark_y][mark_x]==20||map_1[mark_y][mark_x]==0) {
         if (map_mark_1[mark_y-1][mark_x-1]==0) {
             map_mark_1[mark_y-1][mark_x-1]=1; //標記
-            flag_num_1++;
+            game.flag++;
             print_game_easy();
         }
         else if (map_mark_1[mark_y-1][mark_x-1]==1) {
             map_mark_1[mark_y-1][mark_x-1]=0; //清除標記
-            flag_num_1--;
+            game.flag--;
             print_game_easy();
         }
     }
@@ -301,15 +287,15 @@ void print_game_easy() { //介面
     gotoxy(14,1);
     printf("地雷數:10");
     gotoxy(25,1);
-    printf("旗子數:%2d", flag_num_1);
+    printf("旗子數:%2d", game.flag);
     for (i=0;i<LINES_EASY+2;i++) {
         gotoxy(5,i+3);
         for (j=0;j<COLS_EASY+2;j++) {
-            if (map_guess_1[i][j]==99)
+            if (map_1[i][j]==99)
                 printf("■");         //邊界
-            else if (map_guess_1[i][j]==10)
+            else if (map_1[i][j]==10)
                 printf("　");    //踩過的空地
-            else if (map_guess_1[i][j]==0||map_guess_1[i][j]==20) {
+            else if (map_1[i][j]==0||map_1[i][j]==20) {
                 if (map_mark_1[i-1][j-1]==0)
                     printf("□");     //還沒踩過的地面&地雷
                 else if (map_mark_1[i-1][j-1]==1) {
@@ -321,9 +307,9 @@ void print_game_easy() { //介面
                     printf("Χ");     //標記錯誤顯現
                 }
             }
-            else if (map_guess_1[i][j]<10)
-                printf(" %d", map_guess_1[i][j]); //踩過且旁邊有地雷，顯現地雷數
-            else if (map_guess_1[i][j]==21) {
+            else if (map_1[i][j]<10)
+                printf(" %d", map_1[i][j]); //踩過且旁邊有地雷，顯現地雷數
+            else if (map_1[i][j]==21) {
                 if (map_mark_1[i-1][j-1]==0) {
                     color_set(14);
                     printf("※");    //全場地雷處
@@ -333,7 +319,7 @@ void print_game_easy() { //介面
                     printf("◢");    //地雷標記顯現
                 }
             }
-            else if (map_guess_1[i][j]==30) {
+            else if (map_1[i][j]==30) {
                 if (map_mark_1[i-1][j-1]==0) {
                     color_set(11);
                     printf("⊕");    //過關顯示地雷
